80fef4b22b1c9a3dc7fafe9f9c475ff73b08d08e
MertKalkanci <63818504+MertKalkanci@users.noreply.github.com>
2022-03-23 13:50:40 +0300

    tekrarlı oynatma

--- Zil/Zil/Form1.cs
+++ Zil/Zil/Form1.cs
@@ -13,11 +13,33 @@ namespace Zil
 {
     public partial class Form1 : Form
     {
-        private mp3Player mpPlayer;
+        public void mp3Player(string fileName)
+        {
+            const string FORMAT = @"open ""{0}"" type mpegvideo alias MediaFile";
+            string command = String.Format(FORMAT, fileName);
+            mciSendString(command, null, 0, IntPtr.Zero);
+        }
+
         private string z1, z2, im;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        // Declare the nofify constant
+        public const int MM_MCINOTIFY = 953;
+
+        // Override the WndProc function in the form
+        protected override void WndProc(ref Message m)
+        {
+
+            if (m.Msg == MM_MCINOTIFY)
+            {
+                string command = "close MediaFile";
+                mciSendString(command, null, 0, IntPtr.Zero);
+            }
+            base.WndProc(ref m);
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -40,22 +62,25 @@ namespace Zil
         {
             if (im == "" || im == null)
                 return;
-            mpPlayer = new mp3Player(im);
-            mpPlayer.Play();
+            mp3Player(im);
+            string command = "play MediaFile notify";
+            mciSendString(command, null, 0, this.Handle);
         }
         private void z2oButton_Click(object sender, EventArgs e)
         {
             if (z2 == "" || z2 == null)
                 return;
-            mpPlayer = new mp3Player(z2);
-            mpPlayer.Play();
+            mp3Player(z2);
+            string command = "play MediaFile notify";
+            mciSendString(command, null, 0, this.Handle);
         }
         private void z1oButton_Click(object sender, EventArgs e)
         {
             if (z1 == "" || z1 == null)
                 return;
-            mpPlayer = new mp3Player(z1);
-            mpPlayer.Play();
+            mp3Player(z1);
+            string command = "play MediaFile notify";
+            mciSendString(command, null, 0, this.Handle);
         }
         private void ZilSaatleriGuncellemeButton_Click(object sender, EventArgs e)
         {
@@ -66,7 +91,7 @@ namespace Zil
 
         }
 
-
+      
 
         private string openFile(TextBox textBox,string title)
         {
@@ -85,6 +110,7 @@ namespace Zil
             return openFileDialog.FileName;
         }
 
-        
+        [DllImport("winmm.dll")]
+        public static extern long mciSendString(string strCommand, StringBuilder strReturn, int iReturnLength, IntPtr hwndCallBack);
     }
 }
--- Zil/Zil/mp3Player.cs
+++ Zil/Zil/mp3Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Windows.Forms;
 
 namespace Zil
 {
@@ -13,17 +14,17 @@ namespace Zil
             string command = String.Format(FORMAT, fileName);
             mciSendString(command, null, 0, IntPtr.Zero);
         }
-
         public void Play()
         {
-            string command = "play MediaFile";
-            mciSendString(command, null, 0, IntPtr.Zero);
+            string command = "play MediaFile notify";
+            mciSendString(command, null, 0,IntPtr.Zero);
         }
         [DllImport("winmm.dll")]
-        private static extern long mciSendString(string strCommand, StringBuilder strReturn, int iReturnLength, IntPtr hwndCallBack);
+        public static extern long mciSendString(string strCommand, StringBuilder strReturn, int iReturnLength, IntPtr hwndCallBack);
         public void Dispose()
         {
-            string command = "stop MediaFile";
+            string command = "close MediaFile";
+            mciSendString(command, null, 0, IntPtr.Zero);
         }
     }
 }
